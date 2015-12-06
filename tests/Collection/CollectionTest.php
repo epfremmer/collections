dabeed4a009ab651a2e279ffb20aeac2b7cd71c5
epfremme <epfremme@nerdery.com>
2015-12-06 00:44:29 -0600

    Added stub tests for collection & immutable collection classes

--- tests/Collection/CollectionTest.php
+++ tests/Collection/CollectionTest.php
@@ -6,71 +6,23 @@
  */
 namespace Epfremme\Tests\Collection;
 
-use Epfremme\Collection\Collection;
-
 /**
  * Class CollectionTest
  *
  * @package Epfremme\Tests\Collection
  */
-class CollectionTest extends \PHPUnit_Framework_TestCase
+class CollectionTest
 {
-    /**
-     * Test collection construct
-     *
-     * @return void
-     */
-    public function testConstruct()
-    {
-        $collection = new Collection([1,2,3,4,5]);
-
-        $this->assertAttributeInternalType('array', 'elements', $collection);
-        $this->assertAttributeNotEmpty('elements', $collection);
-        $this->assertAttributeCount(5, 'elements', $collection);
-        $this->assertAttributeEquals([1,2,3,4,5], 'elements', $collection);
-    }
-
-    /**
-     * Test empty collection construct
-     *
-     * @return void
-     */
-    public function testConstructEmpty()
-    {
-        $collection = new Collection();
-
-        $this->assertAttributeInternalType('array', 'elements', $collection);
-        $this->assertAttributeEmpty('elements', $collection);
-        $this->assertAttributeCount(0, 'elements', $collection);
-        $this->assertAttributeEquals([], 'elements', $collection);
-    }
-
-    /**
-     * Test associative collection construct
-     *
-     * @return void
-     */
-    public function testConstructAssociative()
-    {
-        $collection = new Collection(['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5]);
-
-        $this->assertAttributeInternalType('array', 'elements', $collection);
-        $this->assertAttributeNotEmpty('elements', $collection);
-        $this->assertAttributeCount(5, 'elements', $collection);
-        $this->assertAttributeEquals(['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5], 'elements', $collection);
-    }
-
-    /**
-     * Test collection is traversable
-     *
-     * @return void
-     */
-    public function testTraversable()
-    {
-        $collection = new Collection([1,2,3,4,5]);
-
-        foreach ($collection as $element) {
-            $this->assertAttributeContains($element, 'elements', $collection);
-        }
-    }
+    public function testPush() {}
+    public function testPop() {}
+    public function testShift() {}
+    public function testUnshift() {}
+    public function testSlice() {}
+    public function testSplice() {}
+    public function testAdd() {}
+    public function testRemove() {}
+    public function testMap() {}
+    public function testSort() {}
+    public function testAsort() {}
+    public function testEach() {}
 }
---
+++ tests/Collection/ImmutableCollectionTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * File ImmutableCollectionTest.php
+ *
+ * @author Edward Pfremmer <epfremme@nerdery.com>
+ */
+namespace Epfremme\Tests\Collection;
+
+/**
+ * Class ImmutableCollectionTest
+ *
+ * @package Epfremme\Tests\Collection
+ */
+class ImmutableCollectionTest
+{
+    public function testOffsetUnset() {}
+    public function testOffsetSet() {}
+}
